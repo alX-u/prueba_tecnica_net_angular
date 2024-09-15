664fc3c8779f87c3f1a4e857e621747d3defd798
alX-u <team4@overnights.com>
2024-09-15 01:24:08 -0500

    add validations to login and signup form

--- PruebaTecnicaNetAngularFrontend/src/app/pages/authentication-page/authentication-page.component.ts
+++ PruebaTecnicaNetAngularFrontend/src/app/pages/authentication-page/authentication-page.component.ts
@@ -6,7 +6,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -21,7 +28,6 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatCheckboxModule,
     MatButtonModule,
-    FormsModule,
     ReactiveFormsModule,
   ],
   templateUrl: './authentication-page.component.html',
@@ -29,16 +35,47 @@ import { CommonModule } from '@angular/common';
 })
 export class AuthenticationPageComponent {
   // Login form
-  emailLogin: string = '';
-  passwordLogin: string = '';
+  loginForm = new FormGroup({
+    emailLogin: new FormControl('', [Validators.required, Validators.email]),
+    passwordLogin: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
   // Register form
-  fullNameRegister: string = '';
-  emailRegister: string = '';
-  passwordRegister: string = '';
-  confirmPasswordRegister: string = '';
+  registerForm = new FormGroup({
+    fullNameRegister: new FormControl('', Validators.required),
+    emailRegister: new FormControl('', [Validators.required, Validators.email]),
+    passwordRegister: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPasswordRegister: new FormControl('', [
+      Validators.required,
+      this.confirmPasswordValidator.bind(this),
+    ]),
+  });
 
-  onLoginSubmit(form: any) {}
+  // Password match validator for register form
+  confirmPasswordValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const form = control.parent as FormGroup;
+    if (!form) {
+      return null;
+    }
+    const password = form.get('passwordRegister')?.value;
+    const confirmPassword = control.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
 
-  onRegisterSubmit(form: any) {}
+  onLoginSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  onRegisterSubmit() {
+    console.log(this.registerForm.errors); // Verifica si el error de validación está presente
+    console.log(this.registerForm.value);
+  }
 }
